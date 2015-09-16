2c3f91edff0cd24287a9c4284454fbfaf8418f8a
Corrie Smith & Michael Summers <student@epicodus.com>
2015-09-16 10:00:09 -0700

    will roll the dice and return a random number

--- spec/specs.js
+++ spec/specs.js
@@ -4,4 +4,8 @@ describe('roll', function() {
     expect(testPlayer.score).to.equal(0);
     expect(testPlayer.currentRoll).to.equal(0);
   });
+  it("rolls the dice", function () {
+    var testPlayer = new Player();
+    expect(testPlayer.roll(1)).not.to.equal([]);
+  });
 });
